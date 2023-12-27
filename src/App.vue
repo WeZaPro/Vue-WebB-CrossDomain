b71da713d1d286818515a800edec6579d1b6957d
WeZaPro <taweesak9359@gmail.com>
2023-12-27 16:19:37 +0700

    up112

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,123 @@
         </li>
       </ul>
     </nav>
-    <h1>WEB SECOND PAGE</h1>
+    <h1>WEB SECOND PAGE App Page Liff App</h1>
     <router-view></router-view>
   </div>
+  <div id="liff">
+    <img :src="profile.pictureUrl" width="60" height="60" />
+    <!-- {{ profile.displayName }} -->
+    <p>display Name : {{ profile.displayName }}</p>
+    <p>userID : {{ profile.userId }}</p>
+    <p>param : {{ this.getParam }}</p>
+
+    <button @click="openLineChat">Line@</button>
+    <button @click="sendMsg">chat</button>
+  </div>
 </template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld.vue";
+import liff from "@line/liff";
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld,
+  },
+  data() {
+    return {
+      loggedIn: false,
+      profile: {},
+      displayName: "",
+      userId: "",
+      alert: "",
+      getParam: "",
+    };
+  },
+  mounted() {
+    //(((((())))))
+    let uri = window.location.search.substring(1);
+    let params = new URLSearchParams(uri);
+    liff
+      .init({ liffId: "1656824759-lQKpOazZ" })
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login();
+        } else {
+          this.loggedIn = liff.isLoggedIn();
+          alert(`Line Login--> `);
+
+          // get queryString
+          this.getParam = params.get("param");
+          console.log("param--->", this.getParam);
+
+          this.getProfile();
+          // this.getEnvironment();
+          this.getFriendship();
+        }
+      })
+      .catch((err) => {
+        this.occoredError = "error:" + err;
+      });
+  },
+  methods: {
+    async getProfile() {
+      await liff.getProfile().then((profile) => {
+        console.log("profile--> ", profile);
+        this.profile = profile;
+        // console.log("this.profile--> ", this.profile);
+      });
+    },
+    async getFriendship() {
+      await liff.getFriendship().then((data) => {
+        console.log("getFriendship--> ", data);
+        if (data.friendFlag) {
+          console.log("getFriendship--> = true");
+          this.alert = data.friendFlag;
+          alert(`data.friendFlag--> ${this.alert}`);
+        }
+      });
+    },
+    async getAccessToken() {
+      console.log("token--> ");
+      await liff.getAccessToken().then((token) => {
+        console.log("token--> ", token);
+      });
+    },
+
+    openLineChat() {
+      console.log("openLineChat--> ");
+      window.open("https://line.me/ti/p/@889mtekm", "_blank");
+    },
+
+    async sendMsg() {
+      const profile = await liff.getProfile();
+      console.log("userId---> " + profile.userId);
+
+      if (!liff.isInClient()) {
+        window.alert(
+          "This button is unavailable as LIFF is currently being opened in an external browser."
+        );
+      } else {
+        await liff
+          .sendMessages([
+            {
+              type: "text",
+              text: `Register/${profile.displayName}`,
+            },
+          ])
+          .then(() => {
+            window.alert(`Message sent Register ` + profile.userId);
+          })
+          .catch((error) => {
+            window.alert("Error sending message: " + error);
+          });
+      }
+    },
+  },
+};
+</script>
 
 <style scoped>
 nav ul {
