de97a00d1485a87ac9ff4ec7d1015a1da254289a
WeZaPro <taweesak9359@gmail.com>
2023-12-27 07:56:44 +0700

    update4

--- src/main.js
+++ src/main.js
@@ -1,27 +1,44 @@
 // import './assets/main.css'
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { createGtm } from "@gtm-support/vue-gtm";
 import { createApp } from "vue";
 import App from "./App.vue";
 import Home from "@/pages/Home.vue";
 import About from "@/pages/About.vue";
 
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: About,
+  },
+];
 const router = createRouter({
-  mode: "history",
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      component: Home,
-    },
-    {
-      path: "/about",
-      component: About,
-    },
-  ],
+  // history: createWebHistory(),
+  history: createWebHashHistory(),
+  routes,
 });
 
-// router.beforeEach((to, from, next) => {
+// const router = createRouter({
+//   mode: "history",
+//   history: createWebHistory(),
+//   routes: [
+//     {
+//       path: "/",
+//       component: Home,
+//     },
+//     {
+//       path: "/about",
+//       component: About,
+//     },
+//   ],
+// });
+
 //   const publicPages = ["/", "/home", "/about"];
 // });
 
