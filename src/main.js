8060a74e4edcc8a6a16d862e2988d5ed28e5d579
WeZaPro <taweesak9359@gmail.com>
2023-12-27 07:24:59 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,40 @@
+// import './assets/main.css'
+import { createRouter, createWebHistory } from "vue-router";
+import { createGtm } from "@gtm-support/vue-gtm";
+import { createApp } from "vue";
+import App from "./App.vue";
+import Home from "./pages/Home.vue";
+import About from "./pages/About.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/about",
+      component: About,
+    },
+  ],
+});
+
+//createApp(App).use(router).mount("#app");
+createApp(App)
+  .use(router)
+  .use(
+    createGtm({
+      id: "GTM-5Q53KCQ6", // GTM ID
+    })
+  )
+  .mount("#app");
+//
+// createApp(App)
+//   .use(
+//     createGtm({
+//       id: "GTM-W4PF2FQ2", // GTM ID
+//       vueRouter: router,
+//     })
+//   )
+//   .mount("#app");
